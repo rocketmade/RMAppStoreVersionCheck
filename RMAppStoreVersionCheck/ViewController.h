dab97e46d29819e2b643a89d73640f9e82e8d4a8
Brandon Roth <brandon@rocketmade.com>
2014-09-04 17:41:58 -0600

    Initial Commit

---
+++ RMAppStoreVersionCheck/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  RMAppStoreVersionCheck
+//
+//  Created by Brandon Roth on 9/4/14.
+//  Copyright (c) 2014 Rocketmade. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ RMAppStoreVersionCheckTests/RMAppStoreVersionCheckTests.m
@@ -0,0 +1,34 @@
+//
+//  RMAppStoreVersionCheckTests.m
+//  RMAppStoreVersionCheckTests
+//
+//  Created by Brandon Roth on 9/4/14.
+//  Copyright (c) 2014 Rocketmade. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface RMAppStoreVersionCheckTests : XCTestCase
+
+@end
+
+@implementation RMAppStoreVersionCheckTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
