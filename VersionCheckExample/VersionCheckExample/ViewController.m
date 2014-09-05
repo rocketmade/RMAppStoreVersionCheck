e0e2a0a924db102cfdb0f41133acbb25b0f74fd5
Brandon Roth <brandon@rocketmade.com>
2014-09-05 10:52:34 -0600

    Add sample app

--- VersionCheckExample/VersionCheckExample/ViewController.m
+++ VersionCheckExample/VersionCheckExample/ViewController.m
@@ -1,14 +1,23 @@
 //
 //  ViewController.m
-//  VersionCheckExample
+//  RMAppStoreVersionCheck
 //
-//  Created by Brandon Roth on 9/5/14.
+//  Created by Brandon Roth on 9/4/14.
 //  Copyright (c) 2014 Rocketmade. All rights reserved.
 //
 
 #import "ViewController.h"
+//#import "RMAppStoreVersionCheck.h"
+//#import "RMAppVersionInformation.h"
 
 @interface ViewController ()
+
+//@property (strong, nonatomic) RMAppStoreVersionCheck *versionCheck;
+@property (weak, nonatomic) IBOutlet UILabel *checkingVersionLabel;
+@property (weak, nonatomic) IBOutlet UIActivityIndicatorView *activityIndicator;
+@property (weak, nonatomic) IBOutlet UILabel *appStoreVersionLabel;
+@property (weak, nonatomic) IBOutlet UILabel *currentVersionLabel;
+@property (weak, nonatomic) IBOutlet UIButton *checkButton;
 
 @end
 
@@ -17,13 +26,31 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    //self.versionCheck = [[RMAppStoreVersionCheck alloc] init];
+    self.currentVersionLabel.hidden = YES;
+    self.appStoreVersionLabel.hidden = YES;
+    self.checkingVersionLabel.hidden = YES;
+    self.activityIndicator.hidden = YES;
 }
 
-- (void)didReceiveMemoryWarning
+- (IBAction)checkButtonPressed:(UIButton *)sender
 {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    self.checkingVersionLabel.hidden = NO;
+    self.activityIndicator.hidden = NO;
+    [self.activityIndicator startAnimating];
+    sender.hidden = YES;
+
+//    [self.versionCheck checkAppStoreVersionForBundleID:@"com.payclip.Clip" completion:^(RMAppVersionInformation *versionInfo, NSError *error) {
+//        self.checkingVersionLabel.hidden = YES;
+//        self.activityIndicator.hidden = YES;
+//        [self.activityIndicator stopAnimating];
+//        sender.hidden = NO;
+//
+//        self.appStoreVersionLabel.hidden = NO;
+//        self.currentVersionLabel.hidden = NO;
+//        self.appStoreVersionLabel.text = [NSString stringWithFormat:@"App Store Version: %@", versionInfo.appStoreVersion];
+//        self.currentVersionLabel.text = [NSString stringWithFormat:@"Current Version: %@", versionInfo.currentVersion];
+//    }];
 }
 
 @end
