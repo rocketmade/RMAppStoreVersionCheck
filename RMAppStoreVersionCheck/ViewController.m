5315efd97b43ae4f8ba4ffc2f4f13983dd01b2da
Brandon Roth <brandon@rocketmade.com>
2014-09-05 10:36:18 -0600

    Add sample to viewController

--- RMAppStoreVersionCheck/ViewController.m
+++ RMAppStoreVersionCheck/ViewController.m
@@ -7,8 +7,17 @@
 //
 
 #import "ViewController.h"
+#import "RMAppStoreVersionCheck.h"
+#import "RMAppVersionInformation.h"
 
 @interface ViewController ()
+
+@property (strong, nonatomic) RMAppStoreVersionCheck *versionCheck;
+@property (weak, nonatomic) IBOutlet UILabel *checkingVersionLabel;
+@property (weak, nonatomic) IBOutlet UIActivityIndicatorView *activityIndicator;
+@property (weak, nonatomic) IBOutlet UILabel *appStoreVersionLabel;
+@property (weak, nonatomic) IBOutlet UILabel *currentVersionLabel;
+@property (weak, nonatomic) IBOutlet UIButton *checkButton;
 
 @end
 
@@ -17,13 +26,31 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    self.versionCheck = [[RMAppStoreVersionCheck alloc] init];
+    self.currentVersionLabel.hidden = YES;
+    self.appStoreVersionLabel.hidden = YES;
+    self.checkingVersionLabel.hidden = YES;
+    self.activityIndicator.hidden = YES;
 }
 
-- (void)didReceiveMemoryWarning
+- (IBAction)checkButtonPressed:(UIButton *)sender
 {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    self.checkingVersionLabel.hidden = NO;
+    self.activityIndicator.hidden = NO;
+    [self.activityIndicator startAnimating];
+    sender.hidden = YES;
+
+    [self.versionCheck checkAppStoreVersionForBundleID:@"com.payclip.Clip" completion:^(RMAppVersionInformation *versionInfo, NSError *error) {
+        self.checkingVersionLabel.hidden = YES;
+        self.activityIndicator.hidden = YES;
+        [self.activityIndicator stopAnimating];
+        sender.hidden = NO;
+
+        self.appStoreVersionLabel.hidden = NO;
+        self.currentVersionLabel.hidden = NO;
+        self.appStoreVersionLabel.text = [NSString stringWithFormat:@"App Store Version: %@", versionInfo.appStoreVersion];
+        self.currentVersionLabel.text = [NSString stringWithFormat:@"Current Version: %@", versionInfo.currentVersion];
+    }];
 }
 
 @end
